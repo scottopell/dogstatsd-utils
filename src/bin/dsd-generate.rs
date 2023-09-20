bbd2830af9ff05d5ead6ab14a1fb60a498284762
Scott Opell <me@scottopell.com>
2023-09-20 17:20:26 -0400

    Utilize lading throttle for rate

--- src/bin/dsd-generate.rs
+++ src/bin/dsd-generate.rs
@@ -1,8 +1,12 @@
+use std::num::NonZeroU32;
+
+use byte_unit::Byte;
+use lading_throttle::Throttle;
 use rand::{rngs::SmallRng, SeedableRng};
 use thiserror::Error;
 
 use clap::Parser;
-use lading_payload::{dogstatsd, Serialize};
+use lading_payload::dogstatsd::{self, KindWeights, MetricWeights, ValueConf};
 
 /// Generate random dogstatsd messages and emit them to stdout line-by-line
 /// if num-msgs is specified, exactly that count of messages will be emitted
@@ -29,19 +33,48 @@ pub enum DSDGenerateError {
     InvalidArgs,
 }
 
-fn main() -> Result<(), DSDGenerateError> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), DSDGenerateError> {
     let args = Args::parse();
 
     if args.num_msgs.is_some() && args.rate.is_some() {
         return Err(DSDGenerateError::InvalidArgs);
-    } else if let Some(num_msgs) = args.num_msgs {
-        let mut rng = SmallRng::seed_from_u64(34512223);
-        let dd = dogstatsd::DogStatsD::default(&mut rng);
+    }
+
+    let mut rng = SmallRng::seed_from_u64(34512423);
+    let dd = dogstatsd::DogStatsD::new(
+        100..500,
+        5..10,
+        5..10,
+        5..10,
+        0..10,
+        1..10,
+        0.08,
+        KindWeights::default(),
+        MetricWeights::default(),
+        ValueConf::default(),
+        &mut rng,
+    );
+    if let Some(num_msgs) = args.num_msgs {
         for _ in 0..num_msgs {
             println!("{}", dd.generate(&mut rng));
         }
+    } else if let Some(rate) = args.rate {
+        let bytes_per_second = Byte::from_str(rate).unwrap().get_bytes() as u32;
+        let mut throttle = Throttle::new_with_config(
+            lading_throttle::Config::default(),
+            NonZeroU32::new(bytes_per_second).unwrap(),
+        );
+        loop {
+            let msg = dd.generate(&mut rng);
+            let msg_str = msg.to_string();
+            let _ = throttle
+                .wait_for(NonZeroU32::new(msg_str.len() as u32).unwrap())
+                .await;
+            println!("{}", msg_str);
+        }
     } else {
-        todo!("Rate not implemented yet.");
+        println!("{}", dd.generate(&mut rng));
     }
 
     Ok(())
